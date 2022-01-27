50b43168ab58d8a408f2d1b2b218329c19518f1c
Shyam Sunder Saravanan <SubstantialCitron0@protonmail.com>
2022-01-27 13:12:01 +0530

    Create avgof3.cpp

---
+++ starters 23 div3/avgof3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main() {
+   int t = 1; std::cin >> t;
+   while(t--){
+   		int x;
+   		std::cin>>x;
+   		std::cout<<x-1<<" "<<x<<" "<<x+1<<"\n";
+
+   }
+   return 0;
+}
